2e5872434fbca7fe8d5504a9e701fe287b3af5cc
oleg dashevskii <olegdashevskii@gmail.com>
2011-02-01 23:55:20 +0600

    Fix capistrano recipe
    (stolen from bundler)

--- lib/githubbish_assets/capistrano.rb
+++ lib/githubbish_assets/capistrano.rb
@@ -1,8 +1,10 @@
-namespace :deploy do
-  desc "Shrink and bundle js and css"
-  task :bundle, :roles => :web, :except => { :no_release => true } do
-    run "cd #{release_path}; RAILS_ROOT=#{release_path} #{fetch(:rake, 'rake')} ghbundle:all RAILS_ENV=#{fetch(:rails_env, 'production')}"
+Capistrano::Configuration.instance(:must_exist).load do
+  namespace :deploy do
+    desc "Shrink and bundle js and css"
+    task :bundle, :roles => :web, :except => { :no_release => true } do
+      run "cd #{release_path}; RAILS_ROOT=#{release_path} #{fetch(:rake, 'rake')} ghbundle:all RAILS_ENV=#{fetch(:rails_env, 'production')}"
+    end
   end
-end
 
-after "deploy:update_code", "deploy:bundle"
+  after "deploy:update_code", "deploy:bundle"
+end
