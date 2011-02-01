4e246803b005cd408be4211c41c50c39437e6997
oleg dashevskii <olegdashevskii@gmail.com>
2011-02-01 23:38:47 +0600

    Add capistrano recipe

---
+++ lib/githubbish_assets/capistrano.rb
@@ -0,0 +1,8 @@
+namespace :deploy do
+  desc "Shrink and bundle js and css"
+  task :bundle, :roles => :web, :except => { :no_release => true } do
+    run "cd #{release_path}; RAILS_ROOT=#{release_path} #{fetch(:rake, 'rake')} ghbundle:all RAILS_ENV=#{fetch(:rails_env, 'production')}"
+  end
+end
+
+after "deploy:update_code", "deploy:bundle"
