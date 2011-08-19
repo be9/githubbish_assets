4e62e5e0dfbc6af8965b929013aae69ffd5fd2c0
oleg dashevskii <olegdashevskii@gmail.com>
2011-08-19 12:01:44 +0700

    Add bundle_locally flag

--- lib/githubbish_assets/capistrano.rb
+++ lib/githubbish_assets/capistrano.rb
@@ -4,7 +4,21 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :bundle, :roles => :web, :except => { :no_release => true } do
       run "cd #{release_path}; RAILS_ROOT=#{release_path} #{fetch(:rake, 'rake')} ghbundle:all RAILS_ENV=#{fetch(:rails_env, 'production')}"
     end
+
+    desc "Shrunk and bundle js and css locally, then upload"
+    task :local_bundle, :roles => :web, :except => { :no_release => true } do
+      puts ">>> Bundling locally"
+      system "bundle exec rake ghbundle:all RAILS_ENV=#{fetch(:rails_env, 'production')}"
+
+      Dir["public/javascripts/bundle_*.js", "public/stylesheets/bundle_*.css"].each do |file|
+        top.upload file, "#{release_path}/#{file}"
+      end
+    end
   end
 
-  after "deploy:update_code", "deploy:bundle"
+  if fetch(:bundle_locally, false)
+    after "deploy:update_code", "deploy:bundle"
+  else
+    after "deploy:update_code", "deploy:local_bundle"
+  end
 end
